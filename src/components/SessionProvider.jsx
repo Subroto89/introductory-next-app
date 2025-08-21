c83fc5dd164eef4b2e314677f5b6171c5a8224b5
Subroto89 <me.subrotosarker@gmail.com>
2025-08-21 18:39:33 +0600

    Basic Setup Completed for Creating Routes

---
+++ src/components/SessionProvider.jsx
@@ -0,0 +1,8 @@
+
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export default function NextAuthSessionProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
