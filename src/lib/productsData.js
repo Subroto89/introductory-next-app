b58bd7cc1fb69f1af1ca1f161f52ef52714f4a0e
Subroto89 <me.subrotosarker@gmail.com>
2025-08-24 16:01:50 +0600

    productsData Updated

--- src/lib/productsData.js
+++ src/lib/productsData.js
@@ -1,28 +1,59 @@
-
-
 // src/lib/productsData.js
 
 const productsData = [
   {
-    id: '1',
-    name: 'Wireless Mouse',
+    id: "1",
+    name: "Wireless Mouse",
     price: 99.99,
-    imageUrl: 'https://trusttechspace.blr1.cdn.digitaloceanspaces.com/media/w1pRH8PVJcMsgWlaEPYo5Rir8w3fvxTjTftSWw8C.jpg',
-    description: 'I can be used for multi tasking with auto easy shortcut, gesture functionality, and it covers 200 metere.',
+    imageUrl:
+      "https://trusttechspace.blr1.cdn.digitaloceanspaces.com/media/w1pRH8PVJcMsgWlaEPYo5Rir8w3fvxTjTftSWw8C.jpg",
+    description:
+      "I can be used for multi tasking with auto easy shortcut, gesture functionality, and it covers 200 metere.",
   },
   {
-    id: '2',
-    name: 'Laser Printer',
-    price: 29.50,
-    imageUrl: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2010/11/printer.jpg?w=600&h=1260&ssl=1',
-    description: 'Canon laser printer. This printer can print 3000 black and white pages and 1000 of color pages with full color accuracy. ',
+    id: "2",
+    name: "Laser Printer",
+    price: 29.5,
+    imageUrl:
+      "https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2010/11/printer.jpg?w=600&h=1260&ssl=1",
+    description:
+      "Canon laser printer. This printer can print 3000 black and white pages and 1000 of color pages with full color accuracy. ",
   },
   {
-    id: '3',
-    name: 'Monitor',
+    id: "3",
+    name: "Monitor",
     price: 19.99,
-    imageUrl: 'https://www.ultratech.com.bd/image/cache/catalog/monitor/samsung/f24t350fhc/samsung-f24t350fhc-24-inch-fhd-monitor-2-500x500.jpg',
-    description: 'Samsung F24T350FHC 24-inch FHD Monitor is very luxurus. It shooths eyes. 120Hz of frequency. 2 hours of electricity backup',
+    imageUrl:
+      "https://www.ultratech.com.bd/image/cache/catalog/monitor/samsung/f24t350fhc/samsung-f24t350fhc-24-inch-fhd-monitor-2-500x500.jpg",
+    description:
+      "Samsung F24T350FHC 24-inch FHD Monitor is very luxurus. It shooths eyes. 120Hz of frequency. 2 hours of electricity backup",
+  },
+  {
+    id: "4",
+    name: "Camera",
+    price: 40,
+    imageUrl:
+      "https://c02.purpledshub.com/uploads/sites/48/2021/07/Canon-EOS-6D-camera-review-07-ee2728d-e1625581210873.jpg?webp=1&w=1200",
+    description:
+      "Canon EOS R6 Mirrorless camera, water proof, print commant using wifi. Store Images directly in the server that is defined",
+  },
+  {
+    id: "5",
+    name: "Headphone",
+    price: 20,
+    imageUrl:
+      "https://www.gonoise.com/cdn/shop/files/1_2807dbd9-3951-481c-9e9a-61af0420cd37.webp?v=1720759165",
+    description:
+      "Airwave Max 4 Wireless Headphone | Up to 70 Hours of Playtime | New Launch | SHOP NOW – Noise, Long time battery backup",
+  },
+    {
+    id: "6",
+    name: "Router",
+    price: 17,
+    imageUrl:
+      "https://images-cdn.ubuy.co.in/633a9d9e80cb794e1d178cdb-tp-link-ac1750-smart-wifi-router-archer.jpg",
+    description:
+      "TP-Link AC1750 Smart WiFi Router Archer A7 -Dual Bangladesh | Ubuy | 200 Meter Range Coverage, Battery Backup upto 3 hours",
   },
 ];
 
